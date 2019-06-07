aace9e1f821fc77d5c64bd2869715d05497bd180
OvercodedStack <vesperiumcreator@gmail.com>
2019-06-07 17:31:09 -0400

    Update 3

---
+++ Project Folder/Assets/Scripts/raycast_collision_19.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+ 
+
+
+public class raycast_collision_19 : MonoBehaviour {
+
+    public float distance_ray;
+    private LineRenderer ir_laser;
+    public GameObject hit_obj; 
+
+	// Use this for initialization
+	void Start () {
+        ir_laser = GetComponent<LineRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        RaycastHit ray;
+        if (Physics.Raycast(transform.position, transform.forward, out ray, distance_ray))
+        {
+            hit_obj = ray.transform.gameObject;
+
+            ir_laser.SetPosition(1, new Vector3(0,0,ray.distance)); 
+        }
+        else
+        { 
+            ir_laser.SetPosition(1, new Vector3(0, 0, distance_ray));
+        }
+	}
+
+    //Simple method to get what was hit for use with other scripts. 
+    GameObject get_obj()
+    {
+        return hit_obj;
+    }
+}
