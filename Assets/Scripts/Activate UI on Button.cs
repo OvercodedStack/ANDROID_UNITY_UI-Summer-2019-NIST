a705b1b0e67c15ecfa234592d0c13b116c458f08
OvercodedStack <vesperiumcreator@gmail.com>
2019-06-30 23:56:31 -0400

    Updated the UI design

---
+++ Assets/Scripts/Activate UI on Button.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class UI_enable_Menu : MonoBehaviour
+{
+
+    public GameObject menu; // Assign in inspector
+    private bool isShowing = false;
+
+    private void Start()
+    {
+        menu.SetActive(isShowing);
+    }
+
+
+    public void hide_or_show()
+    {
+        isShowing = !isShowing;
+        menu.SetActive(isShowing);
+    }
+}
---
+++ Assets/Scripts/UI_enable_Menu.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class MenuAppearScript : MonoBehaviour
+{
+
+    public GameObject menu; // Assign in inspector
+    private bool isShowing;
+
+    void hide_or_show()
+    {
+        isShowing = !isShowing;
+        menu.SetActive(isShowing);
+    }
+}
