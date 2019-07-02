54f9b7023a9e03039f75203077f09aa3a9082d6d
OvercodedStack <vesperiumcreator@gmail.com>
2019-07-02 17:31:05 -0400

    Updated minor items

---
+++ Assets/Scripts/TCP_Client.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+ 
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using UnityEngine;
+
+public class TCP_Client : MonoBehaviour
+{
+    #region private members 	
+    public string IP_address = "169.254.152.150";
+    public int port_num = 27001;
+
+    private TcpClient socketConnection;
+    private Thread clientReceiveThread;
+
+    // Update is called once per frame
+    [Range(0.1F, 20)]
+    public float delay_time = 2.0F;
+    private float last_call = 0.0F;
+
+    #endregion
+    // Use this for initialization 	
+    void Start()
+    {
+        ConnectToTcpServer();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time > last_call)
+        {
+            last_call = Time.time + delay_time;
+            SendMessage();
+        }
+    }
+    /// <summary> 	
+    /// Setup socket connection. 	
+    /// </summary> 	
+    private void ConnectToTcpServer()
+    {
+        try
+        {
+            clientReceiveThread = new Thread(new ThreadStart(ListenForData));
+            clientReceiveThread.IsBackground = true;
+            clientReceiveThread.Start();
+        }
+        catch (Exception e)
+        {
+            Debug.Log("On client connect exception " + e);
+        }
+
+    }
+
+    /// <summary> 	
+    /// Runs in background clientReceiveThread; Listens for incomming data. 	
+    /// </summary>     
+    private void ListenForData()
+    {
+        try
+        {
+            socketConnection = new TcpClient(IP_address, port_num);
+            Byte[] bytes = new Byte[1024];
+            while (true)
+            {
+                // Get a stream object for reading 				
+                using (NetworkStream stream = socketConnection.GetStream())
+                {
+                    int length;
+                    // Read incomming stream into byte arrary. 					
+                    while ((length = stream.Read(bytes, 0, bytes.Length)) != 0)
+                    {
+                        var incommingData = new byte[length];
+                        Array.Copy(bytes, 0, incommingData, 0, length);
+                        // Convert byte array to string message. 						
+                        string serverMessage = Encoding.ASCII.GetString(incommingData);
+                        Console.WriteLine("I got something");
+                        Debug.Log("server message received as: " + serverMessage);
+                    }
+                }
+            }
+        }
+        catch (SocketException socketException)
+        {
+            Console.WriteLine("Something went wrong" + socketException);
+            Debug.Log("Socket exception: " + socketException);
+        }
+    }
+    /// <summary> 	
+    /// Send message to server using socket connection. 	
+    /// </summary> 	
+    private void SendMessage()
+    {
+        if (socketConnection == null)
+        {
+            return;
+        }
+        try
+        {
+            // Get a stream object for writing. 			
+            NetworkStream stream = socketConnection.GetStream();
+            if (stream.CanWrite)
+            {
+                string clientMessage = "This is a message from one of your clients.";
+                // Convert string message to byte array.                 
+                byte[] clientMessageAsByteArray = Encoding.ASCII.GetBytes(clientMessage);
+                // Write byte array to socketConnection stream.                 
+                stream.Write(clientMessageAsByteArray, 0, clientMessageAsByteArray.Length);
+                Debug.Log("Client sent his message - should be received by server");
+            }
+        }
+        catch (SocketException socketException)
+        {
+            Debug.Log("Socket exception: " + socketException);
+        }
+    }
+}
